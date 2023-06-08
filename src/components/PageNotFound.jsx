40b7480681d9d1b28769c56e9594cfcd80155dd0
fraperrone <fperronerey@gmail.com>
2023-06-08 00:40:04 -0300

    Estilos 404 not found

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
+import Image from "react-bootstrap/Image"
 
+import Button from 'react-bootstrap/Button';
 
-export default function PageNotFound(){
-    return(
+export default function PageNotFound() {
+    return (
         <>
-        <h1>Pagina no Encontrada</h1>
-        <Link to="/">Volver al inicio</Link>
+            <div class="row justify-content-center">
+                <div class="col-4">
+                    <h3>Pagina No Encontrada 404 Not Found</h3>
+                    <Image src="../logo192.png" fluid />
+                </div>
+
+            </div>
+
+
+            {/* <Link to="/">Volver al inicio</Link> */}
+
+            <Link to="/">
+                <div class="d-grid gap-2">
+
+                    <button class="btn btn-primary" type="button">Volver al Inicio</button>
+
+                </div>
+            </Link>
+
         </>
     )
 }
