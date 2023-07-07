e1376bfead969fc8c1b1be9611b823eaf36c71e5
fraperrone <fperronerey@gmail.com>
2023-07-07 15:45:13 -0300

    nombre completo show.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Show from './components/Show'
+import Show from './components/Show.jsx'
 import PageNotFound from "./components/PageNotFound";
 //import data from "./data.json";
 import { Create } from "./components/CreateEstudiante"
--- src/components/show.jsx
+++ src/components/show.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebaseConfig/firebase";
